f86d0c26a9f749348591af002b9453e5e9400e93
Alejandro Galindo <alegmed09@gmail.xom>
2020-05-28 23:25:09 -0500

    inital commit

---
+++ dataset.py
@@ -0,0 +1,49 @@
+import albumentations as A
+import pandas as pd
+import numpy as np
+from PIL import Image
+import joblib
+import torch
+from torch.utils.data import DataLoader
+
+class BengaliTrainDataset(DataLoader):
+    def __init__(self, folds, transforms = None):
+        super().__init__()
+
+        df = pd.read_csv("../input/train_folds.csv")
+        df = df[["image_id", "grapheme_root", "vowel_diacritic", "consonant_diacritic", "kfold"]]
+        self.transforms = transforms
+
+        df = df[df.kfold.isin(folds)].reset_index(drop = True)
+        self.image_ids = df.image_id.values
+        self.grapheme_root = df.grapheme_root.values
+        self.vowel_diacritic = df.vowel_diacritic.values
+        self.consonant_diacritic = df.consonant_diacritic.values
+
+    def __getitem__(self, index: int):
+        image = joblib.load(f"../input/image_pickles/{self.image_ids[index]}.pkl")
+        image = image.reshape(137,236).astype(float)
+        image = Image.fromarray(image).convert("RGB")
+        image = np.array(image)
+
+        if self.transforms:
+            sample = {
+              'image': image
+            }
+            sample = self.transforms(**sample)
+            image = sample['image']
+        
+        image = np.transpose(image, (2,0,1)).astype(np.float32)
+
+        return {
+            'image': torch.as_tensor(image, dtype = torch.float),
+            'grapheme_root': torch.as_tensor(self.grapheme_root[index], dtype = torch.long),
+            "vowel_diacritic": torch.as_tensor(self.vowel_diacritic[index], dtype = torch.long),
+            "consonant_diacritic": torch.as_tensor(self.consonant_diacritic[index], dtype = torch.long)
+        }
+
+
+
+    def __len__(self):
+        return len(self.image_ids)
+        
---
+++ models.py
@@ -0,0 +1,27 @@
+import pretrainedmodels
+import torch.nn as nn
+from torch.nn import functional as F
+
+class ResNet34(nn.Module):
+    def __init__(self, pretrained):
+        super(ResNet34, self).__init__()
+
+        if pretrained is True:
+            self.model = pretrainedmodels.__dict__["resnet34"](pretrained="imagenet")
+        else:
+            self.model = pretrainedmodels.__dict__["resnet34"](pretrained=None)
+
+        #initialize target layers
+        self.l0 = nn.Linear(512,168) #grapheme root
+        self.l1 = nn.Linear(512,11) #vowel diacritic
+        self.l2 = nn.Linear(512,7) #consonant diacritic
+
+    def forward(self, x):
+        bs,_,_,_=x.shape
+        x = self.model.features(x)
+        x = F.adaptive_avg_pool2d(x, 1).reshape(bs, -1)
+        l0 = self.l0(x)
+        l1 = self.l1(x)
+        l2 = self.l2(x)
+
+        return l0,l1,l2
